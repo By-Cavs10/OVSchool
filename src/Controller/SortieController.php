a7b8de3cf9c8cc218f68ea602a7b972b97111a41
By-Cavs10 <clementdossantos.10@gmail.com>
2024-11-06 14:42:23 +0100

    API + form

--- src/Controller/SortieController.php
+++ src/Controller/SortieController.php
@@ -4,9 +4,11 @@ namespace App\Controller;
 
 use App\Entity\Sortie;
 use App\Form\SortieType;
+use App\Helper\FileUploader;
 use App\Repository\SortieRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -39,17 +41,26 @@ class SortieController extends AbstractController
     }
 
     #[Route('/edit', name: '_edit')]
-    public function create(Request $request, EntityManagerInterface $em): Response
+    public function create(Request $request, EntityManagerInterface $em, FileUploader $fileUploader): Response
     {
         $sortie = new Sortie();
         $form = $this->createForm(SortieType::class, $sortie);
         $form->handleRequest($request);
 
-        if ($form->isSubmitted()) {
-            $sortie->setNom($sortie->getNom());
-            $sortie->setDateHeureDebut($sortie->getDateHeureDebut());
+        if ($form->isSubmitted() && $form->isValid()) {
+            $file = $form->get('urlPhoto')->getData();
+//            $sortie->setNom($sortie->getNom());
+//            $sortie->setDateHeureDebut($sortie->getDateHeureDebut());
+
+            if ($file instanceof UploadedFile) {
+                $name = $fileUploader->uploadPhoto($file, $sortie->getNom(), 'upload_directory_sorties');
+                $sortie->setUrlPhoto($name);
+            }
+
             $em->persist($sortie);
             $em->flush();
+
+
             $this->addFlash('sucess', 'Sortie bien créé');
             return $this->redirectToRoute('sortie_list');
         }
---
+++ src/EntityListener/SortieListener.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\EntityListener;
+
+use App\Entity\Sortie;
+use App\Entity\User;
+use App\Repository\SortieRepository;
+use Doctrine\ORM\EntityManagerInterface;
+
+class SortieListener{
+
+
+
+}
